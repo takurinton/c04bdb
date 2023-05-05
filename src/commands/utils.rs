87276b08a99daefd3309073e70f8cb9cbeb7c74b
takurinton <takurinton@takurinton.dev>
2023-05-06 07:45:39 +0900

    fix: utils

--- src/commands/utils.rs
+++ src/commands/utils.rs
@@ -116,3 +116,35 @@ pub async fn github_search(language: &str) -> Result<Vec<GithubTrendItem>, Strin
 
     Ok(body.items)
 }
+
+fn hex_to_u8(hex: u8) -> u8 {
+    match hex {
+        b'0'..=b'9' => hex - b'0',
+        b'a'..=b'f' => hex - b'a' + 10,
+        b'A'..=b'F' => hex - b'A' + 10,
+        _ => panic!("Invalid hex digit: {}", hex),
+    }
+}
+
+pub fn percent_decode(input: &str) -> String {
+    use std::str;
+
+    let mut out = Vec::new();
+    let mut i = 0;
+    let bytes = input.as_bytes();
+    while i < bytes.len() {
+        match bytes[i] {
+            b'%' if i + 2 < bytes.len() => {
+                let h = hex_to_u8(bytes[i + 1]);
+                let l = hex_to_u8(bytes[i + 2]);
+                out.push((h << 4) | l);
+                i += 3;
+            }
+            b => {
+                out.push(b);
+                i += 1;
+            }
+        }
+    }
+    str::from_utf8(&out).unwrap().to_string()
+}
--- src/commands/wiki.rs
+++ src/commands/wiki.rs
@@ -6,7 +6,7 @@ use serenity::model::prelude::interaction::application_command::{
     CommandDataOption, CommandDataOptionValue,
 };
 
-use crate::commands::utils::google_search;
+use crate::commands::utils::{google_search, percent_decode};
 
 #[derive(Deserialize)]
 struct Pages {
@@ -23,38 +23,6 @@ struct Query {
 #[derive(Deserialize)]
 struct Response {
     query: Query,
-}
-
-fn hex_to_u8(hex: u8) -> u8 {
-    match hex {
-        b'0'..=b'9' => hex - b'0',
-        b'a'..=b'f' => hex - b'a' + 10,
-        b'A'..=b'F' => hex - b'A' + 10,
-        _ => panic!("Invalid hex digit: {}", hex),
-    }
-}
-
-fn percent_decode(input: &str) -> String {
-    use std::str;
-
-    let mut out = Vec::new();
-    let mut i = 0;
-    let bytes = input.as_bytes();
-    while i < bytes.len() {
-        match bytes[i] {
-            b'%' if i + 2 < bytes.len() => {
-                let h = hex_to_u8(bytes[i + 1]);
-                let l = hex_to_u8(bytes[i + 2]);
-                out.push((h << 4) | l);
-                i += 3;
-            }
-            b => {
-                out.push(b);
-                i += 1;
-            }
-        }
-    }
-    str::from_utf8(&out).unwrap().to_string()
 }
 
 pub async fn run(options: &[CommandDataOption]) -> String {
