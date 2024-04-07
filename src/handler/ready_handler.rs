de70412e3ee00cc3bda74e60987950c9a4bbe0d4
takurinton <takurinton@takurinton.dev>
2024-04-07 14:07:04 +0900

    chore refactor scheduler (#19)
    define trait
    
    pub async fn get_last_date
    
    impl rss processer
    
    add run function
    
    fix call main.rs
    
    readly_handler
    
    fix
    
    fix

--- src/handler/ready_handler.rs
+++ src/handler/ready_handler.rs
@@ -1,13 +1,10 @@
 use std::env;
 
-use crate::commands;
-use crate::utils::fetch_rss_feed::fetch_rss_feed;
-use crate::utils::get_db_channel::get_db_channel;
-use serenity::{
-    client::Context,
-    model::id::{ChannelId, GuildId},
-};
-use tracing::{error, info, warn};
+use crate::scheduler::rss::ProcesserStruct;
+use crate::{commands, scheduler::processer::Processer};
+
+use serenity::{client::Context, model::id::GuildId};
+use tracing::info;
 
 pub async fn ready(ctx: Context) {
     let guild_id = GuildId(889012300705591307);
@@ -30,59 +27,9 @@ pub async fn ready(ctx: Context) {
 
     let mode = env::var("RUST_ENV").unwrap_or_else(|_| "development".to_string());
 
-    if mode == "production" {
-        info!("RSS retrieval is started.");
-        let feeds = match fetch_rss_feed(&ctx).await {
-            Ok(feeds) => feeds,
-            Err(why) => {
-                error!("Error fetching RSS feed: {:?}", why);
-                vec![]
-            }
-        };
-
-        // channel に post
-        let channel = ChannelId(1208611584964825099);
-        for feed in feeds {
-            let _ = match feed.link {
-                // link が存在したらそのまま送信する
-                Some(link) => {
-                    let _ = channel.send_message(&ctx.http, |m| m.content(link)).await;
-                }
-                // 何もなかったら何もしない
-                None => {
-                    warn!("No link found in RSS feed: {:?}", feed.title);
-                }
-            };
-        }
-
-        let db_channel = match get_db_channel(&ctx).await {
-            Ok(db_channel) => db_channel,
-            Err(why) => {
-                error!("Error getting db channel: {:?}", why);
-                return;
-            }
-        };
-
-        let messages = db_channel
-            .messages(&ctx.http, |retriever| retriever.limit(1))
-            .await
-            .unwrap()
-            .into_iter()
-            .filter(|message| message.content.starts_with("rss_last_date"))
-            .collect::<Vec<_>>();
-
-        for message in messages {
-            let _ = message.delete(&ctx.http).await;
-        }
-
-        // 日付を更新
-        let now = chrono::Utc::now();
-        let now = now.format("%Y-%m-%d %H:%M:%S").to_string();
-        let _ = db_channel
-            .send_message(&ctx.http, |m| m.content(format!("rss_last_date {}", now)))
-            .await;
-
-        info!("RSS retrieval is done.");
+    if mode == "development" {
+        let rss_processor = ProcesserStruct;
+        rss_processor.run(&ctx).await.unwrap();
     } else {
         info!("RSS retrieval is not performed in development mode.");
     }
