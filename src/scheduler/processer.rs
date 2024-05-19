83e44af0664ef40f61f0293e4e9f7088520dbd42
takurinton <takurinton@takurinton.dev>
2024-05-19 20:30:51 +0900

    fix scheduler types

--- src/scheduler/processer.rs
+++ src/scheduler/processer.rs
@@ -1,12 +1,11 @@
-use rss::Item;
 use serenity::async_trait;
 use serenity::client::Context;
 use std::error::Error;
 
 #[async_trait]
-pub trait Processer {
-    async fn fetch(&self, ctx: &Context) -> Result<Vec<Item>, Box<dyn Error>>;
-    async fn post_to_channel(&self, ctx: &Context, items: Vec<Item>) -> Result<(), Box<dyn Error>>;
+pub trait Processer<T> {
+    async fn fetch(&self, ctx: &Context) -> Result<Vec<T>, Box<dyn Error>>;
+    async fn post_to_channel(&self, ctx: &Context, items: Vec<T>) -> Result<(), Box<dyn Error>>;
     async fn update_db_channel(&self, ctx: &Context) -> Result<(), Box<dyn Error>>;
     async fn run(&self, ctx: &Context) -> Result<(), Box<dyn Error>>;
 }
